cb16512c3085a3be93233de521e2eeed6e13ea8e
zeyad12-d <zeadyasser054@gmail.com>
2024-10-26 07:09:04 +0300

    task3

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace day2_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // task 3
+
+            int sum = 0;
+            
+            while (sum <= 100)
+            {
+                Console.WriteLine("enter a number you have sum");
+                int number=int.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine($" you sum {sum} is exceeds 100 now");
+
+        }
+    }
+}
